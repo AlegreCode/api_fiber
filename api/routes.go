a943f19f5bd3edf8abb9a3d4fff89e775506c0f6
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2023-03-19 22:33:07 -0300

    Definimos la ruta Post author y lo vinculamos con la función CreateAutthor de authorController.

--- api/routes.go
+++ api/routes.go
@@ -3,8 +3,12 @@ package api
 import (
 	"github.com/alegrecode/api_fiber/controllers"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 )
 
-func Router(app *fiber.App) {
-	app.Get("/", controllers.AuthorController)
+func Router(app *fiber.App, db *gorm.DB) {
+
+	authorController := controllers.AuthorController(db)
+
+	app.Post("/author", authorController.CreateAuthor)
 }
