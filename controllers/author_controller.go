8f026bb6bbec3299572a363c39e9d27012e31a57
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2023-03-19 22:36:55 -0300

    Creamos la función AuthorController, el objeto AuthorHandler  y la función CreateAuthor.
    La función AuthorController recibe una instancia de la conexión a la base de datos y retorna un AuthorHandler.
    La función CreateAuthor extiende el objeto AuthorHandler y crea un nuevo author en la base de datos y retorna un objeto JSON con los datos guardados.

--- controllers/author_controller.go
+++ controllers/author_controller.go
@@ -1,7 +1,31 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
 
-func AuthorController(c *fiber.Ctx) error {
-	return c.SendString("Hello World!")
+	"github.com/alegrecode/api_fiber/models"
+)
+
+func AuthorController(db *gorm.DB) *AuthorHandler {
+	return &AuthorHandler{db}
+}
+
+type AuthorHandler struct {
+	db *gorm.DB
+}
+
+func (ah *AuthorHandler) CreateAuthor(c *fiber.Ctx) error {
+
+	author := new(models.Author)
+
+	if err := c.BodyParser(author); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Could not create author",
+			"error":   err.Error(),
+		})
+	}
+
+	ah.db.Create(&author)
+	return c.JSON(author)
 }
