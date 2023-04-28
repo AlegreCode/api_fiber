3e09e502740bcf41e006d18a261dc2c6de5f60b2
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2023-04-28 18:21:27 -0300

    Añadimos las funciones para recuperar los datos de un author, actualizar y borrar.

--- controllers/author_controller.go
+++ controllers/author_controller.go
@@ -35,3 +35,62 @@ func (ah *AuthorHandler) CreateAuthor(c *fiber.Ctx) error {
 	ah.db.Create(&author)
 	return c.JSON(author)
 }
+
+func (ah *AuthorHandler) GetAuthor(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var author models.Author
+	ah.db.First(&author, id)
+
+	if author.ID == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "Author not found",
+		})
+	}
+
+	return c.JSON(author)
+}
+
+func (ah *AuthorHandler) UpdateAuthor(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var author models.Author
+	ah.db.First(&author, id)
+
+	if author.ID == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "Author not found",
+		})
+	}
+
+	updatedAuthor := new(models.Author)
+
+	if err := c.BodyParser(&updatedAuthor); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Could not update author",
+			"error":   err.Error(),
+		})
+	}
+
+	author.Name = updatedAuthor.Name
+	author.Lastname = updatedAuthor.Lastname
+	ah.db.Save(&author)
+
+	return c.JSON(author)
+}
+
+func (ah *AuthorHandler) DeleteAuthor(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var author models.Author
+	ah.db.First(&author, id)
+
+	if author.ID == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "Author not found",
+		})
+	}
+
+	ah.db.Delete(&author)
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Author deleted",
+	})
+}
