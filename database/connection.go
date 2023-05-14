a5c5fc8b50ae23d6f773d77e23bc76b6eda8c481
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2023-05-14 20:58:49 -0300

    Corregimos error: debemos utilizar la variable DB declarada fuera de la función, por lo que no se debe declarar nuevamente dentro de la función.

--- database/connection.go
+++ database/connection.go
@@ -7,13 +7,9 @@ import (
 
 var DB *gorm.DB
 
-func ConnectDB() (*gorm.DB, error) {
+func ConnectDB() *gorm.DB {
 	dsn := "root@tcp(127.0.0.1:3306)/api_fiber?charset=utf8mb4&parseTime=True&loc=Local"
-	DB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
-	if err != nil {
-		return nil, err
-	}
-
-	return DB, nil
+	return DB
 }
